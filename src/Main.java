34ee1638269afaab05d6c12d614b8a2f70d2e262
VIMPER1 <bulik-71s@mail.ru>
2026-09-02 23:37:53 +0500

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+void main(String[] args) {
+        // Задание 1
+        System.out.println("Имя: Максим, Возраст: 19, Город: Караганда");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+        // Задание 2
+        int age = 19;
+        String name = "Максим";
+        double height = 1.68;
+        char symbol = 'B';
+        boolean male = true;
+
+        // Задание 3
+        for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+        }
+
+        // Задание 4
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " * 7 = " + (i * 7));
+        }
+
 }
